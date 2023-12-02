8415856326d4b13cf8dd083ddd9287ad6ad95dbf
Dean Sorie <66969624+deanosaurx@users.noreply.github.com>
2023-12-02 21:07:24 +0200

    Update projects.tf

--- 01-administration/projects.tf
+++ 01-administration/projects.tf
@@ -3,7 +3,7 @@ module "org_audit_logs_project" {
   version                 = "~> 14.1"
   random_project_id       = true
   default_service_account = "deprivilege"
-  name                    = "central-audit-logging"
+  name                    = "central-audit-logging-v2"
   org_id                  = var.org_id
   billing_account         = var.billing_account_id
   folder_id               = 2988491520
@@ -11,7 +11,7 @@ module "org_audit_logs_project" {
 
   labels = {
     environment       = "production"
-    application_name  = "org-logging"
+    application_name  = "org-logging-v2"
     billing_code      = "1234"
     primary_contact   = "example1"
     secondary_contact = "example2"
@@ -33,7 +33,7 @@ module "billing_logs_project" {
 
   labels = {
     environment       = "production"
-    application_name  = "org-billing-logs"
+    application_name  = "org-billing-logs-v2"
     billing_code      = "1234"
     primary_contact   = "example1"
     secondary_contact = "example2"
